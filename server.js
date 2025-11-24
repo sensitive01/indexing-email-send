2ec73cc59d5f5c2416f95361a1f891e1e50d081f
Aswinrajr <aswinrajr07@gmail.com>
2025-11-24 18:51:08 +0530

    serveice account added

--- server.js
+++ server.js
@@ -140,3 +140,10 @@ app.post("/contact", async (req, res) => {
     res.status(500).json({ success: false, message: "Internal server error." });
   }
 });
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
